9ce2a6214d2d39aa3f48bf609a7b760e90f6abad
Mengxiao Qi <mengxiaoqi@MacBook-Air.local>
2021-10-15 00:56:07 -0500

    add app.post with database

--- server.js
+++ server.js
@@ -10,16 +10,28 @@ const db = mysql.createConnection(
           password: '820902qmx@NJ',
           database: 'movie_db'
      },
-     console.log(`Connected to the classlist_db database.`)
+     console.log(`Connected to the movie_db database.`)
 );
+app.use(express.json());
 
-
-db.query('SELECT * FROM movies', function (err, results) {
-     console.log(results);
+app.get('/api/movies', (req, res) => {
+     db.query('SELECT * FROM movies', function (err, result) {
+          // return JSON.stringify(result)
+          //res.send('1') --> for insomnia
+          res.send(result)
+     });
 });
 
-
-
+app.post('/api/add-movie', (req, res) => {
+     const newMovie = req.body.name;
+     console.log(newMovie);
+     db.query(`INSERT INTO movies (movie_name) VALUES ('${newMovie}')`, (err, result) => {
+          if(err) {console.error(err)}
+          else {
+               res.send('movie added')
+          }
+     });
+});
 
 
 
